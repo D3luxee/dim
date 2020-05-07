14226db6006a14bddb6f6027f33f26a241bcb348
Georg Doser <georg@neuland.tech>
2020-05-07 11:33:30 +0200

    WIP stuff

--- docker/entrypoint-dim.sh
+++ docker/entrypoint-dim.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #configure based on the env variables
-sed -r -i "s/^[# ]*gmysql-host=.*/gmysql-host=${MYSQL_HOST}/g" /etc/pdns/pdns.conf
-sed -r -i "s/^[# ]*gmysql-port=.*/gmysql-port=${MYSQL_PORT}/g" /etc/pdns/pdns.conf
-sed -r -i "s/^[# ]*gmysql-user=.*/gmysql-user=${MYSQL_USER}/g" /etc/pdns/pdns.conf
-sed -r -i "s/^[# ]*gmysql-password=.*/gmysql-password=${MYSQL_PASS}/g" /etc/pdns/pdns.conf
-sed -r -i "s/^[# ]*gmysql-dbname=.*/gmysql-dbname=${MYSQL_DB}/g" /etc/pdns/pdns.conf
-sed -r -i "s/^[# ]*gmysql-dnssec=.*/gmysql-dnssec=${MYSQL_DNSSEC}/g" /etc/pdns/pdns.conf
+sed -i "s+sqlalchemy+${SQLALCHEMY_DATABASE_URI}+g" /etc/dim/dim.cfg
+sed -i "s+authmethod+${AUTHENTICATION_METHOD}+g" /etc/dim/dim.cfg
+sed -i "s+testkey+${SECRET_KEY}+g" /etc/dim/dim.cfg
+#sed -r -i "s/^[# ]*gmysql-user=.*/gmysql-user=${MYSQL_USER}/g" /etc/pdns/pdns.conf
+#sed -r -i "s/^[# ]*gmysql-password=.*/gmysql-password=${MYSQL_PASS}/g" /etc/pdns/pdns.conf
+#sed -r -i "s/^[# ]*gmysql-dbname=.*/gmysql-dbname=${MYSQL_DB}/g" /etc/pdns/pdns.conf
+#sed -r -i "s/^[# ]*gmysql-dnssec=.*/gmysql-dnssec=${MYSQL_DNSSEC}/g" /etc/pdns/pdns.conf
 
-
+cat /etc/dim/dim.cfg
 #initialize db
 /dim/manage_db init
 #start dim
